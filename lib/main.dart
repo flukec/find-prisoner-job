88b86acaf72e21535e86d5af4e262ee45eff72f6
tonkla41 <tonkla41@gmail.com>
2021-12-12 12:45:50 +0700

    [FIX] import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'dart:async';
-import 'dart:html';
+// import 'dart:async';
+// import 'dart:html';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
